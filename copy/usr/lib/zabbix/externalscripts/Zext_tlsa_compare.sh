7044921e094b892780c3e6698fe239a8771cf0a4
jfqd <jfqd@blun.org>
2022-04-14 10:25:00 +0200

    add parameter-support

--- copy/usr/lib/zabbix/externalscripts/Zext_tlsa_compare.sh
+++ copy/usr/lib/zabbix/externalscripts/Zext_tlsa_compare.sh
@@ -1,9 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+set -o pipefail
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-SHOULD=$(ldns-dane -d create qutic.com 443 | awk '{ print $8 }' | tr '[:lower:]' '[:upper:]')
-REAL=$(dig +short TLSA _25._tcp.mx1.qutic.com | awk '{ print $4$5 }' | tr '[:lower:]' '[:upper:]')
+if test -z "$1" ; then
+  echo "You need to supply a DOMAIN to check. Quitting"
+  exit 1;
+fi
+if test -z "$2" ; then
+  echo "You need to supply a SUBDOMAIN to check. Quitting"
+  exit 1;
+fi
+if test -z "$3" ; then
+  echo "You need to supply a PORT to check. Quitting"
+  exit 1;
+fi
+
+DOMAIN=$1
+SUBDOMAIN=$2
+PORT=$3
+
+SHOULD=$(ldns-dane -d create ${DOMAIN} 443 | awk '{ print $8 }' | tr '[:lower:]' '[:upper:]')
+REAL=$(dig +short TLSA _${PORT}._tcp.${SUBDOMAIN}.${DOMAIN} | awk '{ print $4$5 }' | tr '[:lower:]' '[:upper:]')
 
 EXIT=0
 if [ "${SHOULD}" = "${REAL}" ]; then
